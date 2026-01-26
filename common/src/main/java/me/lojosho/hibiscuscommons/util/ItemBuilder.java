20e2417c69a8ce4d3b3332854db9388f96159afd
Logan <37521985+LoJoSho@users.noreply.github.com>
2026-01-26 14:29:08 -0600

    fix: modify ItemBuilder to use newer methods in 1.21.4

--- common/src/main/java/me/lojosho/hibiscuscommons/util/ItemBuilder.java
+++ common/src/main/java/me/lojosho/hibiscuscommons/util/ItemBuilder.java
@@ -48,13 +48,16 @@ public class ItemBuilder {
     }
 
     public ItemBuilder setDisplayName(@NotNull String displayName) {
-        if (onPaper) itemMeta.displayName(AdventureUtils.MINI_MESSAGE.deserialize(displayName));
+        if (onPaper) setDisplayName(AdventureUtils.MINI_MESSAGE.deserialize(displayName));
         else itemMeta.setDisplayName(StringUtils.parseStringToString(displayName));
         return this;
     }
 
     public ItemBuilder setDisplayName(@NotNull Component displayName) {
-        if (onPaper) itemMeta.displayName(displayName);
+        if (onPaper) {
+            if (NMSHandlers.getVersion().isHigherOrEqual(MinecraftVersion.v1_21_4)) itemMeta.customName(displayName);
+            else itemMeta.displayName(displayName);
+        }
         return this;
     }
 
