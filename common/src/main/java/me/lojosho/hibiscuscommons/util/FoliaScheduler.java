8d29c57adba3a116d07180715c9a13fe79f52ed5
JosTheDude <jos@joscodes.xyz>
2026-04-07 09:08:54 -0400

    feat: migrate execution call to use paper-api instead of reflection

--- common/src/main/java/me/lojosho/hibiscuscommons/util/FoliaScheduler.java
+++ common/src/main/java/me/lojosho/hibiscuscommons/util/FoliaScheduler.java
@@ -5,10 +5,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.plugin.Plugin;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.function.Consumer;
-
 public final class FoliaScheduler {
 
     private FoliaScheduler() {
@@ -21,12 +17,8 @@ public final class FoliaScheduler {
             return;
         }
 
-        try {
-            Object scheduler = entity.getClass().getMethod("getScheduler").invoke(entity);
-            Method runDelayed = scheduler.getClass().getMethod("runDelayed", Plugin.class, Consumer.class, Runnable.class, long.class);
-            runDelayed.invoke(scheduler, plugin, (Consumer<Object>) scheduledTask -> task.run(), null, delayTicks);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException exception) {
-            throw new IllegalStateException("Failed to schedule entity task on Folia.", exception);
+        if (!entity.getScheduler().execute(plugin, task, null, delayTicks)) {
+            throw new IllegalStateException("Failed to schedule entity task on Folia.");
         }
     }
 }
